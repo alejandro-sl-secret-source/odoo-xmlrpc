2692112a789f028062e3129cf640a87d131cd4d6
Alejandro Serrano <alejandro.eurojobsites@gmail.com>
2026-05-25 14:57:07 +0100

    Update Odoo client instantiation

--- src/Odoo.php
+++ src/Odoo.php
@@ -4,6 +4,8 @@ namespace AlazziAz\OdooXmlrpc;
 
 use AlazziAz\OdooXmlrpc\Contracts\OdooClientContract;
 use AlazziAz\OdooXmlrpc\Enums\EndPoints;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\HttpFactory as GuzzlePsr7Factory;
 use Laminas\XmlRpc\Client;
 
 class Odoo
@@ -13,9 +15,21 @@ class Odoo
      */
     public static function client(string $url, string $suffix, string $db, string $username, string $password): OdooClientContract
     {
+        $psr17  = new GuzzlePsr7Factory();
+        $httpClient   = new GuzzleClient();
 
-        $commonClient = new Client(EndPoints::Common->getFullUrl($url, $suffix));
-        $objectClient = new Client(EndPoints::Object->getFullUrl($url, $suffix));
+        $commonClient = new Client(
+            EndPoints::Common->getFullUrl($url, $suffix),
+            $httpClient,
+            $psr17,
+            $psr17
+        );
+        $objectClient = new Client(
+            EndPoints::Object->getFullUrl($url, $suffix),
+            $httpClient,
+            $psr17,
+            $psr17
+        );
 
         return new OdooClient(
             commonClient: $commonClient,
